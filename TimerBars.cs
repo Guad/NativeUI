cb7a2b7526ed7d1dd4c3e9304005d5e57d711cc4
Guad <rockeurp@gmail.com>
2016-05-25 19:53:06 +0200

    Added movable TimerBarPool, fixes #33

--- TimerBars.cs
+++ TimerBars.cs
@@ -9,19 +9,19 @@ namespace NativeUI
     public abstract class TimerBarBase
     {
         public string Label { get; set; }
-
+        
         public TimerBarBase(string label)
         {
             Label = label;
         }
 
-        public virtual void Draw(int interval)
+        public virtual void Draw(int interval, Size offset)
         {
             SizeF res = UIMenu.GetScreenResolutionMantainRatio();
-            Point safe = UIMenu.GetSafezoneBounds();
+            Point safe = UIMenu.GetSafezoneBounds() + offset;
 
-            new UIResText(Label, new Point((int)res.Width - safe.X - 180, (int)res.Height - safe.Y - (30 + (4 * interval))), 0.3f, Color.White, Font.ChaletLondon, UIResText.Alignment.Right).Draw();
-            new Sprite("timerbars", "all_black_bg", new Point((int)res.Width - safe.X - 298, (int)res.Height - safe.Y - (40 + (4 * interval))), new Size(300, 37), 0f, Color.FromArgb(180, 255, 255, 255)).Draw();
+            new UIResText(Label, new Point((int)res.Width - safe.X - 180, (int)res.Height - safe.Y - (30 + (interval))), 0.3f, Color.White, Font.ChaletLondon, UIResText.Alignment.Right).Draw();
+            new Sprite("timerbars", "all_black_bg", new Point((int)res.Width - safe.X - 298, (int)res.Height - safe.Y - (40 + (interval))), new Size(300, 37), 0f, Color.FromArgb(180, 255, 255, 255)).Draw();
 
 			Screen.HideHudComponentThisFrame(HudComponent.AreaName);
 			Screen.HideHudComponentThisFrame(HudComponent.StreetName);
@@ -38,13 +38,13 @@ namespace NativeUI
             Text = text;
         }
 
-        public override void Draw(int interval)
+        public override void Draw(int interval, Size offset)
         {
             SizeF res = UIMenu.GetScreenResolutionMantainRatio();
-            Point safe = UIMenu.GetSafezoneBounds();
+            Point safe = UIMenu.GetSafezoneBounds() + offset;
 
-            base.Draw(interval);
-            new UIResText(Text, new Point((int)res.Width - safe.X - 10, (int)res.Height - safe.Y - (42 + (4 * interval))), 0.5f, Color.White, Font.ChaletLondon, UIResText.Alignment.Right).Draw();
+            base.Draw(interval, offset);
+            new UIResText(Text, new Point((int)res.Width - safe.X - 10, (int)res.Height - safe.Y - (42 + (interval))), 0.5f, Color.White, Font.ChaletLondon, UIResText.Alignment.Right).Draw();
         }
     }
 
@@ -66,14 +66,14 @@ namespace NativeUI
             ForegroundColor = Color.Red;
         }
 
-        public override void Draw(int interval)
+        public override void Draw(int interval, Size offset)
         {
             SizeF res = UIMenu.GetScreenResolutionMantainRatio();
-            Point safe = UIMenu.GetSafezoneBounds();
+            Point safe = UIMenu.GetSafezoneBounds() + offset;
 
-            base.Draw(interval);
+            base.Draw(interval, offset);
 
-            var start = new Point((int)res.Width - safe.X - 160, (int)res.Height - safe.Y - (28 + (4 * interval)));
+            var start = new Point((int)res.Width - safe.X - 160, (int)res.Height - safe.Y - (28 + (interval)));
 
             new UIResRectangle(start, new Size(150, 15), BackgroundColor).Draw();
             new UIResRectangle(start, new Size((int)(150 * Percentage), 15), ForegroundColor).Draw();
@@ -82,7 +82,9 @@ namespace NativeUI
 
     public class TimerBarPool
     {
-        private static List<TimerBarBase> _bars = new List<TimerBarBase>();
+        private List<TimerBarBase> _bars;
+        
+        public Size Offset { get; set; }
 
         public TimerBarPool()
         {
@@ -108,7 +110,7 @@ namespace NativeUI
         { 
             for (int i = 0; i < _bars.Count; i++)
             {
-                _bars[i].Draw(i * 10);
+                _bars[i].Draw(i * 40, Offset);
             }
         }
     }
