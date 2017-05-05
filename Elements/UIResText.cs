a97cab95e1486198e48b6572601a043602badc4c
Guad <rockeurp@gmail.com>
2017-05-05 23:38:47 +0200

    organized stuff into folders. Namespaces left untouched for backwards compatibility

--- Elements/UIResText.cs
+++ Elements/UIResText.cs
@@ -47,6 +47,16 @@ namespace NativeUI
             }
         }
 
+        public static void AddLongString(CallCollection collection, string str)
+        {
+            const int strLen = 99;
+            for (int i = 0; i < str.Length; i += strLen)
+            {
+                string substr = str.Substring(i, Math.Min(strLen, str.Length - i));
+                collection.Call(Hash._ADD_TEXT_COMPONENT_STRING, substr);
+            }
+        }
+
 
         public static float MeasureStringWidth(string str, Font font, float scale)
         {
@@ -69,6 +79,8 @@ namespace NativeUI
 
         public override void Draw(Size offset)
         {
+            var coll = new CallCollection();
+
             int screenw = Game.ScreenResolution.Width;
             int screenh = Game.ScreenResolution.Height;
             const float height = 1080f;
@@ -77,36 +89,38 @@ namespace NativeUI
 
             float x = (Position.X) / width;
             float y = (Position.Y) / height;
-            
-            Function.Call(Hash.SET_TEXT_FONT, (int)Font);
-            Function.Call(Hash.SET_TEXT_SCALE, 1.0f, Scale);
-            Function.Call(Hash.SET_TEXT_COLOUR, Color.R, Color.G, Color.B, Color.A);
+
+            coll.Call(Hash.SET_TEXT_FONT, (int)Font);
+            coll.Call(Hash.SET_TEXT_SCALE, 1.0f, Scale);
+            coll.Call(Hash.SET_TEXT_COLOUR, Color.R, Color.G, Color.B, Color.A);
             if (DropShadow)
-                Function.Call(Hash.SET_TEXT_DROP_SHADOW);
+                coll.Call(Hash.SET_TEXT_DROP_SHADOW);
             if (Outline)
-                Function.Call(Hash.SET_TEXT_OUTLINE);
+                coll.Call(Hash.SET_TEXT_OUTLINE);
             switch (TextAlignment)
             {
                 case Alignment.Centered:
-                    Function.Call(Hash.SET_TEXT_CENTRE, true);
+                    coll.Call(Hash.SET_TEXT_CENTRE, true);
                     break;
                 case Alignment.Right:
-                    Function.Call(Hash.SET_TEXT_RIGHT_JUSTIFY, true);
-                    Function.Call(Hash.SET_TEXT_WRAP, 0, x);
+                    coll.Call(Hash.SET_TEXT_RIGHT_JUSTIFY, true);
+                    coll.Call(Hash.SET_TEXT_WRAP, 0, x);
                     break;
             }
 
             if (WordWrap != new Size(0, 0))
             {
                 float xsize = (Position.X + WordWrap.Width)/width;
-                Function.Call(Hash.SET_TEXT_WRAP, x, xsize);
+                coll.Call(Hash.SET_TEXT_WRAP, x, xsize);
             }
 
-            Function.Call(Hash._SET_TEXT_ENTRY, "jamyfafi");
-            AddLongString(Caption);
-            
+            coll.Call(Hash._SET_TEXT_ENTRY, "jamyfafi");
+            AddLongString(coll, Caption);
 
-            Function.Call(Hash._DRAW_TEXT, x, y);
+
+            coll.Call(Hash._DRAW_TEXT, x, y);
+
+            coll.Execute();
         }
 
         public enum Alignment
