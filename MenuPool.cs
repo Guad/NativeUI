b19c2c5b66a097b4817bafb8081b6c206c6d47da
Hannele Ruiz <justlemoncl@gmail.com>
2018-12-06 19:28:34 -0300

    Making AddSubMenu a single function

--- MenuPool.cs
+++ MenuPool.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
 using Control = GTA.Control;
@@ -60,12 +61,7 @@ namespace NativeUI
         /// <returns>The newly created submenu.</returns>
         public UIMenu AddSubMenu(UIMenu menu, string text)
         {
-            var item = new UIMenuItem(text);
-            menu.AddItem(item);
-            var submenu = new UIMenu(menu.Title.Caption, text);
-            this.Add(submenu);
-            menu.BindMenuToItem(submenu, item);
-            return submenu;
+            return AddSubMenu(menu, text, "", Point.Empty);
         }
 
         /// <summary>
@@ -77,16 +73,9 @@ namespace NativeUI
         /// <param name="text">The name of the submenu</param>
         /// <param name="offset">The offset of the menu</param>
         /// <returns>The newly created submenu.</returns>
-        public UIMenu AddSubMenu(UIMenu menu, string text, System.Drawing.Point offset)
+        public UIMenu AddSubMenu(UIMenu menu, string text, Point offset)
         {
-            var item = new UIMenuItem(text);
-            menu.AddItem(item);
-            var submenu = new UIMenu(menu.Title.Caption, text, offset);
-            
-            this.Add(submenu);
-            menu.BindMenuToItem(submenu, item);
-
-            return submenu;
+            return AddSubMenu(menu, text, "", offset);
         }
 
         /// <summary>
@@ -100,10 +89,24 @@ namespace NativeUI
         /// <returns>The newly created submenu.</returns>
         public UIMenu AddSubMenu(UIMenu menu, string text, string description)
         {
-            var item = new UIMenuItem(text, description);
+            return AddSubMenu(menu, text, description, Point.Empty);
+        }
+
+        /// <summary>
+        /// Create and add a submenu to the menu pool.
+        /// Adds an item with the given text and description to the menu, creates a corresponding submenu, and binds the submenu to the item.
+        /// The submenu inherits its title from the menu, and its subtitle from the item text.
+        /// </summary>
+        /// <param name="menu">The parent menu to which the submenu must be added.</param>
+        /// <param name="text">The name of the submenu.</param>
+        /// <param name="description">The name of the submenu.</param>
+        /// <returns>The newly created submenu.</returns>
+        public UIMenu AddSubMenu(UIMenu menu, string text, string description, Point offset)
+        {
+            UIMenuItem item = new UIMenuItem(text, description);
             menu.AddItem(item);
-            var submenu = new UIMenu(menu.Title.Caption, text);
-            this.Add(submenu);
+            UIMenu submenu = new UIMenu(menu.Title.Caption, text, offset);
+            Add(submenu);
             menu.BindMenuToItem(submenu, item);
             return submenu;
         }
