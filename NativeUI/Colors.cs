093276ef4aef1afbafc65dd99f69590940e18cce
Hannele Ruiz <justlemoncl@gmail.com>
2019-02-21 03:23:26 -0300

    Added Colors required by NativeUI
    They are not present on the FiveM implementation of System.Drawing

---
+++ NativeUI/Colors.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace NativeUI
+{
+    public static class Colors
+    {
+        public static readonly Color Black = Color.FromArgb(-16777216);
+        public static readonly Color DarkRed = Color.FromArgb(-7667712);
+        public static readonly Color DodgerBlue = Color.FromArgb(-14774017);
+        public static readonly Color Red = Color.FromArgb(-65536);
+        public static readonly Color White = Color.FromArgb(-1);
+        public static readonly Color WhiteSmoke = Color.FromArgb(-657931);
+    }
+}
