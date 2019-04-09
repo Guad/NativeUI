9211d33ccd1f707bdde39399d27bb845f1f5039b
Hannele Ruiz <justlemoncl@gmail.com>
2019-04-09 09:22:30 -0400

    Making the new NativeText compatible with UIElement

--- NativeUI/Elements/NativeText.cs
+++ NativeUI/Elements/NativeText.cs
@@ -1,9 +1,37 @@
-﻿namespace NativeUI.Elements
+﻿using GTA;
+using System.Drawing;
+
+namespace NativeUI.Elements
 {
     /// <summary>
     /// New Design for the NativeUI texts.
     /// </summary>
-    public class NativeText
+    public class NativeText : UIElement
     {
+        /// <summary>
+        /// If the text is enabled or not.
+        /// </summary>
+        public bool Enabled { get; set; }
+        /// <summary>
+        /// The position of the text
+        /// </summary>
+        public Point Position { get; set; }
+        /// <summary>
+        /// The color of the text.
+        /// </summary>
+        public Color Color { get; set; }
+
+        /// <summary>
+        /// Draws the text on screen.
+        /// </summary>
+        public void Draw()
+        {
+
+        }
+
+        /// <summary>
+        /// Draws the text on screen with the specified offset.
+        /// </summary>
+        public void Draw(Size offset) => Draw();
     }
 }
