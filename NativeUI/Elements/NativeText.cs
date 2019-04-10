3942aa493be0fdc5d187478002acd94325285321
Hannele Ruiz <justlemoncl@gmail.com>
2019-04-10 16:57:47 -0400

    Added AddLongStringForAscii contents into our new Draw

--- NativeUI/Elements/NativeText.cs
+++ NativeUI/Elements/NativeText.cs
@@ -1,6 +1,8 @@
 ﻿using GTA;
 using GTA.Native;
+using System;
 using System.Drawing;
+using System.Text;
 
 namespace NativeUI.Elements
 {
@@ -160,8 +162,26 @@ namespace NativeUI.Elements
                 Function.Call(Hash.SET_TEXT_WRAP, _Relative.X, CalculatedWrap);
             }
 
-            // Add the text
+            // Start adding the text
             Function.Call(Hash._SET_TEXT_ENTRY, "jamyfafi");
+            // If the UTF-8 byte coint matches the total text length
+            if (Encoding.UTF8.GetByteCount(Caption) == Caption.Length)
+            {
+                // Iterate over the string length as long as is not over 99
+                for (int i = 0; i < Caption.Length; i += 99)
+                {
+                    // Create a substring based on a random value
+                    string SubString = Caption.Substring(i, Math.Min(99, Caption.Length - i));
+                    // And add that string
+                    Function.Call(Hash._ADD_TEXT_COMPONENT_STRING, SubString);
+                }
+            }
+            else
+            {
+                // TODO from AddLongStringForUtf8
+            }
+            // And draw it
+            Function.Call(Hash._DRAW_TEXT, _Relative.X, _Relative.Y);
         }
 
         #endregion
