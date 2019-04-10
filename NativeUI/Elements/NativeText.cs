962957cd85b337983da436445d21ee7eb1275f67
Hannele Ruiz <justlemoncl@gmail.com>
2019-04-10 15:38:51 -0400

    Added enum for the text alignment

--- NativeUI/Elements/NativeText.cs
+++ NativeUI/Elements/NativeText.cs
@@ -1,17 +1,52 @@
-﻿using GTA;
+using GTA;
 using System.Drawing;
 
 namespace NativeUI.Elements
 {
     /// <summary>
+    /// The alignment of the text to draw.
+    /// </summary>
+    public enum TextAlignment
+    {
+        Left,
+        Centered,
+        Right,
+    }
+
+    /// <summary>
     /// New Design for the NativeUI texts.
     /// </summary>
     public class NativeText : UIElement
     {
+        #region Private Properties
+
+        /// <summary>
+        /// Our internal caption.
+        /// </summary>
+        private string _Caption = "";
+
+        #endregion
+
+        #region Public Properties
+
         /// <summary>
         /// If the text is enabled or not.
         /// </summary>
         public bool Enabled { get; set; }
+        /// <summary>
+        /// The UTF-8 string to draw on the screen.
+        /// </summary>
+        public string Caption
+        {
+            get
+            {
+                return _Caption;
+            }
+            set
+            {
+                _Caption = value;
+            }
+        }
         /// <summary>
         /// The position of the text
         /// </summary>
