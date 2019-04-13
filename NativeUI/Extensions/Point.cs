9061edb7134a28cb8f3300ce065629664b255c6c
Hannele Ruiz <justlemoncl@gmail.com>
2019-04-13 17:04:18 -0400

    Added comment on the point exception

--- NativeUI/Extensions/Point.cs
+++ NativeUI/Extensions/Point.cs
@@ -10,10 +10,15 @@ namespace NativeUI.Extensions
         /// </summary>
         public static PointF ToRelative(this Point point, out float Width)
         {
+            // Get the Width and Heigth of the current resolution
             float W = Game.ScreenResolution.Width;
             float H = Game.ScreenResolution.Height;
+            // Calculate the ratio of the current resolution
+            // In other words, the height percentage relative to the width
             float Ratio = W / H;
+            // Get the real width
             Width = 1080f * Ratio;
+            // Then, return the correctly calculated PointF
             return new PointF(point.X / Width, point.Y / 1080f);
         }
     }
