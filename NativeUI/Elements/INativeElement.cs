1cc93cd01fdd050b9d068580981068fa7071a5a0
Hannele Ruiz <justlemoncl@gmail.com>
2019-08-26 09:25:20 -0400

    Added interface for all NativeUI elements

---
+++ NativeUI/Elements/INativeElement.cs
@@ -0,0 +1,20 @@
+﻿using GTA;
+using System.Drawing;
+
+namespace NativeUI.Elements
+{
+    /// <summary>
+    /// Interface that should be implemented with all NativeUI elements.
+    /// </summary>
+    public interface INativeElement : UIElement
+    {
+        /// <summary>
+        /// The size of the element.
+        /// </summary>
+        Size Size { get; set; }
+        /// <summary>
+        /// The rotation or heading of the element.
+        /// </summary>
+        float Rotation { get; set; }
+    }
+}
